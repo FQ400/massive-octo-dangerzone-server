052f817e8ab526c6e8a0fa595597085d1855452c
Torsten Schmits <ts@buddybrand.de>
2013-01-18 16:35:02 +0100

    control access to GameObject's effect-susceptible parameters through method_missing()

--- ruby/game_object.rb
+++ ruby/game_object.rb
@@ -1,8 +1,8 @@
 class GameObject
-  attr_accessor :id, :icon, :position, :direction, :angle, :speed, :direction, :size, :hp, :visible, :effects
+  attr_accessor :id, :position, :effects
 
   def initialize(options)
-    options = {
+    @options = {
       angle: 0,
       direction: Vector[0,0],
       hp: 10000,
@@ -15,11 +15,7 @@ class GameObject
       ttl: -1,
       visible: true
     }.merge(options)
-    
-    options.each do |key, value|
-      instance_variable_set("@#{key.to_s}", value)
-    end
-    
+
     @id = rand(10000000)
     @start_position = @position 
     @spawn_time = Time.now.to_f
@@ -48,14 +44,19 @@ class GameObject
     }
   end
 
-  def speed
-    apply_effects('speed')
+  # TODO respond_to anpassen
+  def method_missing(name, *args, &block)
+    if @options.has_key?(name)
+      args.count > 0 ? @options[name] = args[0] : apply_effects(name)
+    else
+      super
+    end
   end
 
   def apply_effects(attr)
-    value = instance_variable_get(('@' + attr).to_sym)
-    return value if @effects[attr.to_sym].nil?
-    @effects[attr.to_sym].each do |effect|
+    value = @options[attr]
+    return value if @effects[attr].nil?
+    @effects[attr].each do |effect|
       value = effect.apply(value)
     end
     value
